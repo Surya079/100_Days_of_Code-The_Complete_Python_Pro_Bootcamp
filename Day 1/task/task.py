b2dd48b7673eb7b2e8c511bf5a2e1e8487c60637
Surya079-V2 <Surya@hazhtech.com>
2025-05-26 22:54:53 +0530

    Day 1 band name created

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,5 +1,4 @@
 print("Welcome to Band Name generator")
 city = input("Enter your City name?\n")
 name = input("Enter your name ?\n")
-
 print("Congratulations! Your band name was generated : " + name +city)
