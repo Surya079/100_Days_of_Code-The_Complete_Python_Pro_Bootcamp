f322df13c07e17192a2ddc2b50a314c51bca9d29
Surya079 <developer.surya03@gmail.com>
2025-05-29 22:25:04 +0530

    Day 3 progress

---
+++ Day 3/If Else/task.py
@@ -0,0 +1,3 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+print("You can") if height > 160 else print("You can't")
---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,6 @@
+print(10 % 3)
+num = int(input("Enter Number"))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("odd")
